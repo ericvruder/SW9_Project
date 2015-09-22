f1373cd1e9ef8b12c8d8ffbad6ee637ec9dadbd0
Eric Ruder <ericvruder@gmail.com>
2015-09-22 10:46:19 +0200

    added emgu

--- SW9_Project/ImageBlogDetectionWindow.xaml.cs
+++ SW9_Project/ImageBlogDetectionWindow.xaml.cs
@@ -20,5 +20,27 @@ namespace SW9_Project {
         public ImageBlogDetectionWindow() {
             InitializeComponent();
         }
+
+        private void selectImageButton_Click(object sender, RoutedEventArgs e) {
+
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+            string filename;
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true) {
+                filename = dlg.FileName;
+            }
+            else {
+                filename = "testing.png";
+            }
+        }
     }
 }
