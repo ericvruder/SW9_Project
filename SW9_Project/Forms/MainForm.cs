80040969dcd78806407e69291a1b89bc371b666f
Eric Ruder <ericvruder@gmail.com>
2015-10-09 14:36:58 +0200

    added tcp listener

--- SW9_Project/Forms/MainForm.cs
+++ SW9_Project/Forms/MainForm.cs
@@ -4,12 +4,20 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SW9_Project {
     public partial class MainForm : Form {
+
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
+
+        [DllImport("Kernel32")]
+        public static extern void FreeConsole();
+
         public MainForm() {
             InitializeComponent();
         }
@@ -17,6 +25,12 @@ namespace SW9_Project {
         private void button1_Click(object sender, EventArgs e) {
             Form objectTracker = new ObjectTrackerForm();
             objectTracker.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            AllocConsole();
+            Console.WriteLine("Testing");
+            Server t = new Server();
         }
     }
 }
---
+++ SW9_Project/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW9_Project {
+    class User {
+        static List<User> allUsers;
+        public static List<User> AllUsers {
+            get {
+                if(allUsers == null) {
+                    allUsers = new List<User>();
+                }
+                return allUsers;
+            }
+        }
+
+        private Socket userSocket = null;
+        private bool userAlive = true;
+
+        public User() {
+            AllUsers.Add(this);
+        }
+
+        public void AddMobileConnection(Socket socketConnection) {
+            userSocket = socketConnection;
+            Task.Factory.StartNew(() => { ManageMobileConnection(); });
+        }
+
+        private void ManageMobileConnection(){
+            try {
+                Console.WriteLine("User connected! Address: " + userSocket.RemoteEndPoint);
+                NetworkStream stream = new NetworkStream(userSocket);
+                StreamReader sr = new StreamReader(stream);
+                StreamWriter sw = new StreamWriter(stream);
+                sw.AutoFlush = true;
+                sw.WriteLine("Received your connection!");
+                while (true) {
+                    string readLine = sr.ReadLine();
+                    if (readLine == "quit") { break; }
+                    Console.WriteLine(readLine);
+                }
+                sw.Close();
+                sr.Close();
+                stream.Close();
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            } finally {
+                userSocket.Close();
+            }
+        }
+    }
+}
