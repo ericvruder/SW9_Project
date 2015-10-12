b7ed40beb10232569939d9c93612bc65a7b096ae
Eric Ruder <ericvruder@gmail.com>
2015-10-12 10:53:23 +0200

    beginning to work on json object parser

---
+++ SW9_Project/MobileData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace SW9_Project {
+    class MobileData {
+
+        public string Name { get; set; }
+        public string TeamName { get; set; }
+        public string Email { get; set; }
+
+        public MobileData(string jsonString) {
+            JObject t = JObject.Parse(jsonString);
+            JToken token = t["user"];
+            Name = (string)t["name"];
+            TeamName = (string)t["teamname"];
+            Email = (string)t["email"];
+        }
+    }
+}
