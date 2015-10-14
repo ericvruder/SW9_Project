882795df43c814edc38d7d20136305e14525f644
Elias <eliasringhauge@hotmail.com>
2015-10-14 10:16:07 +0200

    All your base is belong to Blarh

---
+++ Blarh/Delete_junk.bat
@@ -0,0 +1,14 @@
+echo off
+rem the next line deletes only files and NOT directories.
+del /S "*.bbl"
+del /S "*.blg"
+del /S "*.log"
+del /S "*.out"
+del /S "*.toc"
+del /S "*.aux"
+del /S "*.lox"
+del /S "*.tps"
+del /S "*.tdo"
+del /S "*.synctex.gz"
+del /S "*.gz*"
+del /S "*.sty"
---
+++ Blarh/win_compile-MikTeknonstop.bat
@@ -0,0 +1,51 @@
+﻿:: et script til at compile pdf filer, fra main.tex til main-%date% (system datoen
+:: på dit system) der er også et antal linjer som sletter alle de små filer
+:: pdflatex skaber når den bliver kørt.
+:: Filen kan sættes til at køre sletningen alene, eller til at beholde filerne.
+:: TODO:
+:: --help/-h/-? kommandoer dog indeholder filen stadig kun meget lidt og skulle være til at læse så problemet er ikke stort.
+:: tjek om main.tex faktisk findes...
+:: brug andre filer -f/--file
+@echo off
+
+if "%1" == "-c" goto :cleanup
+if "%1" == "--cleanup" goto :cleanup
+
+pdflatex rapport.tex -job-name=main-%date% -interaction=nonstopmode
+:: bibtex skal køres to gange, på filen af uvisse grunde.
+bibtex main-%date%
+bibtex main-%date%
+pdflatex rapport.tex -job-name=main-%date% -interaction=nonstopmode
+bibtex main-%date%
+pdflatex rapport.tex -job-name=main-%date% -interaction=nonstopmode
+
+if "%1" == "-nc" goto :END
+if "%1" == "--nocleanup" goto :END
+
+:cleanup
+del /F *.out
+del /F *.aux
+del /F *.bbl
+del /F *.blg
+del /F *.txt
+del /F *.synctex
+del /F *.toc
+del /F *.log
+del /F *.gz
+del /F *.gz(busy)
+
+del /S "*.bbl"
+del /S "*.blg"
+del /S "*.log"
+del /S "*.out"
+del /S "*.toc"
+del /S "*.aux"
+del /S "*.lox"
+del /S "*.tps"
+del /S "*.tdo"
+del /S "*.synctex.gz"
+del /S "*.gz*"
+del /S "*.sty"
+
+
+:END
