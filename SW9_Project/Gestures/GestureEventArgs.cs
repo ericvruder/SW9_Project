c26955c1bd7bbdf52e3301509c01b458714252fa
bmla <detailx@gmail.com>
2015-10-27 17:00:02 +0100

    Collected all gesture related files to Gesture folder

---
+++ SW9_Project/Gestures/GestureEventArgs.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW9_Project
+{
+    /// <summary>
+    /// The gesture event arguments
+    /// </summary>
+    public class GestureEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GestureEventArgs"/> class.
+        /// </summary>
+        /// <param name="type">The gesture type.</param>
+        /// <param name="trackingID">The tracking ID.</param>
+        public GestureEventArgs(string name, int trackingId, SkeletonPoint pos)
+        {
+            this.TrackingId = trackingId;
+            this.GestureName = name;
+            this.Position = pos;
+            this.Time = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Gets or sets the type of the gesture.
+        /// </summary>
+        /// <value>
+        /// The name of the gesture.
+        /// </value>
+        public string GestureName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tracking ID.
+        /// </summary>
+        /// <value>
+        /// The tracking ID.
+        /// </value>
+        public int TrackingId { get; set; }
+
+        public SkeletonPoint Position { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
---
+++ SW9_Project/Gestures/Techniques/SwingRightSegments.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW9_Project
+{
+    public class SwingRightSegment1 : IRelativeGestureSegment
+    {
+        public GesturePartResult CheckGesture(Skeleton skeleton)
+        {
+            if (skeleton.Joints[JointType.HandLeft].Position.Y > skeleton.Joints[JointType.HipLeft].Position.Y) {
+                // If left hand is pointing
+                if (skeleton.Joints[JointType.HandLeft].Position.Z < skeleton.Joints[JointType.ElbowLeft].Position.Z)
+                {
+                    if (skeleton.Joints[JointType.ElbowLeft].Position.Z < skeleton.Joints[JointType.ShoulderLeft].Position.Z)
+                    {
+                        // If right hand is ready to make a gesture towards the screen
+                        if (skeleton.Joints[JointType.HandRight].Position.Z > skeleton.Joints[JointType.ShoulderLeft].Position.Z - 0.1)
+                        {
+                            return GesturePartResult.Succeed;
+                        }
+                    }
+                    return GesturePartResult.Pausing;
+                    // hand has not dropped but is not quite where we expect it to be, pausing till next frame
+                }
+                return GesturePartResult.Pausing;
+            }
+            // left hand is not pointing at the screen
+            return GesturePartResult.Fail;
+        }
+    }
+
+    public class SwingRightSegment2 : IRelativeGestureSegment
+    {
+        public GesturePartResult CheckGesture(Skeleton skeleton)
+        {
+            if (skeleton.Joints[JointType.HandLeft].Position.Y > skeleton.Joints[JointType.HipLeft].Position.Y)
+            {
+                // If left hand is pointing
+                if (skeleton.Joints[JointType.HandLeft].Position.Z < skeleton.Joints[JointType.ElbowLeft].Position.Z)
+                {
+                    if (skeleton.Joints[JointType.ElbowLeft].Position.Z < skeleton.Joints[JointType.ShoulderLeft].Position.Z)
+                    {
+                        // If right hand is moving towards the screen
+                        if (skeleton.Joints[JointType.HandRight].Position.Z < skeleton.Joints[JointType.ShoulderLeft].Position.Z - 0.1)
+                        {
+                            return GesturePartResult.Succeed;
+                        }
+                    }
+                    return GesturePartResult.Pausing;
+                    // hand has not dropped but is not quite where we expect it to be, pausing till next frame
+                }
+                return GesturePartResult.Pausing;
+            }
+            // Left hand is not pointing at the screen
+            return GesturePartResult.Fail;
+        }
+    }
+}
+
