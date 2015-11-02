d36b96b0e7d83f7608211f952e6c438c4dd67fa2
bmla <detailx@gmail.com>
2015-11-02 15:26:15 +0100

    Saves to a default log file if no path is given

--- SW9_Project/Logging/Logger.cs
+++ SW9_Project/Logging/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,59 @@ namespace SW9_Project.Logging
     class Logger
     {  
 
-        public void LogGesture(string gestureName, Point pointer, DateTime time)
+        private static string logFilePath;
+
+        public Logger(): this("log-default.txt") { }
+        public Logger(string filePath)
         {
-            string message = "GESTURE" +
+            logFilePath = filePath;
+        }
+
+        public static string filePath
+        {
+            get { return Logger.logFilePath; }
+            set { if (value.Length > 0) Logger.logFilePath = value; }
+        }
+
+        public void LogKinectGesture(string gestureName, Point pointer, DateTime time)
+        {
+            string message = "KINECT GESTURE" +
                              "\t name: " + gestureName +
                              "\t x,y: " + pointer +
                              "\t time: " + time;
 
+            log(message);
+
             Console.WriteLine(message);
         }
+
+        public void LogMobileGesture(string gestureName, DateTime time)
+        {
+            string message = "MOBILE GESTURE" +
+                             "\t name: " + gestureName +
+                             "\t time: " + time;
+
+            log(message);
+
+            Console.WriteLine(message);
+        }
+
+        public static void log(string msg)
+        {
+            if (msg.Length > 0)
+            {
+                using (StreamWriter sw = File.AppendText(Logger.filePath))
+                {
+                    sw.WriteLine("Log time [{0} {1}]: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToLongTimeString(), msg);
+                    sw.Flush();
+                }
+            }
+        }
+
+        public static void flush()
+        {
+            File.WriteAllText(Logger.filePath, string.Empty);
+        }
+
     }
 }
