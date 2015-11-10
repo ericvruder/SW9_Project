57be59ef7281fd6b7a289f150c422a257ff5d5a8
bmla <detailx@gmail.com>
2015-11-10 16:15:28 +0100

    Shapes added. And can now send selected shape for swipe and pinch

--- Android/app/src/main/java/com/nui/android/PinchGestureListener.java
+++ Android/app/src/main/java/com/nui/android/PinchGestureListener.java
@@ -1,5 +1,6 @@
 package com.nui.android;
 
+import android.util.Log;
 import android.view.ScaleGestureDetector;
 
 /**
@@ -8,10 +9,12 @@ import android.view.ScaleGestureDetector;
 public class PinchGestureListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
     IServer server;
     SwipeGestureListener swiper;
+    MainActivity mainActivity;
     private long lastUpdate = 0;
-    public PinchGestureListener(IServer server, SwipeGestureListener swiper){
+    public PinchGestureListener(IServer server, SwipeGestureListener swiper, MainActivity mainActivity){
         this.server = server;
         this.swiper = swiper;
+        this.mainActivity = mainActivity;
     }
     @Override
     public boolean onScale(ScaleGestureDetector detector){
@@ -19,7 +22,8 @@ public class PinchGestureListener extends ScaleGestureDetector.SimpleOnScaleGest
         if(curTime - lastUpdate > 500) {
             lastUpdate = curTime;
             swiper.Pinching();
-            server.SendData(new PinchGesture("circle"));
+            server.SendData(new PinchGesture(mainActivity.GetSelectedShape()));
+            Log.d("PINCH", "OnScale() " + mainActivity.GetSelectedShape());
         }
         return true;
     }
