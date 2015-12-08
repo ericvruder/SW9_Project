1a8c0d7828b7f6f55db0014472ea7b48258e5b47
bmla <detailx@gmail.com>
2015-12-08 14:54:28 +0100

    GetUserInfoTechnique uses dot instead of comma for percentage and added GetAllTechniqueAttempts()

--- DataSetGenerator/DataGenerator.cs
+++ DataSetGenerator/DataGenerator.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.IO;
 
 using WebDataParser;
-
+using System.Globalization;
 
 namespace DataSetGenerator {
 
@@ -24,9 +24,31 @@ namespace DataSetGenerator {
                     foreach(var gesture in t.Attempts) {
                         string time = (t.Attempts[gesture.Key].Last().Time - t.TestStart[gesture.Key]).TotalSeconds.ToString();
                         float hitPercentage = Test.GetHitsPerTry(t.Attempts[gesture.Key]).Last() * 100f;
-                        string totalHit = hitPercentage.ToString();
-                        string totalError = (100f - hitPercentage).ToString();
+                        string totalHit = hitPercentage.ToString(CultureInfo.InvariantCulture);
+                        string totalError = (100f - hitPercentage).ToString(CultureInfo.InvariantCulture);
                         datawriter.WriteLine(t.ID + ", " + gesture.Key + ", " + time + ", " + totalHit + ", " + totalError);
+                    }
+                }
+            }
+        }
+
+        public static void GetAllTechniqueAttempts()
+        {
+            using (StreamWriter datawriter = new StreamWriter("all_technique_data.csv"))
+            {
+                string[] files = Directory.GetFiles(TestFileDirectory, "*.test");
+                foreach (var file in files)
+                {
+                    string id = file.Split('/').Last().Split('.')[0];
+                    Test t = new Test(new StreamReader(file), id);
+                    foreach(var attempt in t.Attempts)
+                    {
+                        foreach(var a in t.Attempts[attempt.Key])
+                        {
+                            string type = attempt.Key.ToString();
+                            string hit = a.Hit ? "1" : "0";
+                            datawriter.WriteLine(type + ", " + hit);
+                        }
                     }
                 }
             }
