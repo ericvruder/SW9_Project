fc116b1fbda976ccc3ba3f515d52ddb04c927f78
Eric Ruder <ericvruder@gmail.com>
2016-01-11 16:53:35 +0100

    dunno wtf git laver

--- DataSetGenerator/DataGenerator.cs
+++ DataSetGenerator/DataGenerator.cs
@@ -499,6 +499,98 @@ namespace DataSetGenerator {
             DrawHitBox(sth.ToList(), "throwsmall.png");
 
         }
+       
+        /*
+        function sqr(x) { return x * x }
+        function dist2(v, w) { return sqr(v.x - w.x) + sqr(v.y - w.y) }
+        function distToSegmentSquared(p, v, w) {
+          var l2 = dist2(v, w);
+          if (l2 == 0) return dist2(p, v);
+          var t = ((p.x - v.x) * (w.x - v.x) + (p.y - v.y) * (w.y - v.y)) / l2;
+          if (t < 0) return dist2(p, v);
+          if (t > 1) return dist2(p, w);
+          return dist2(p, { x: v.x + t * (w.x - v.x),
+                            y: v.y + t * (w.y - v.y) });
+        }
+        function distToSegment(p, v, w) { return Math.sqrt(distToSegmentSquared(p, v, w)); }
+        */
 
+        private static double DistanceSquare(WebDataParser.Point v, WebDataParser.Point w) {
+            return Math.Pow(v.X - w.X, 2) + Math.Pow(v.Y - w.Y,2);
+        }
+
+        private static double DistanceToSegmentSquared(WebDataParser.Point p, WebDataParser.Point v, WebDataParser.Point w) {
+            double l2 = DistanceSquare(v, w);
+            if(l2 == 0) { return DistanceSquare(p, v); }
+            double t = ((p.X - v.X) * (w.X - v.X) + (p.Y - v.Y) * (w.Y - v.Y)) / l2;
+            if(t < 0) { return DistanceSquare(p, v); }
+            if(t > 1) { return DistanceSquare(p, w); }
+            WebDataParser.Point n = new WebDataParser.Point(v.X + t * (w.X - v.X), v.Y + t * (w.Y - v.Y));
+            return DistanceSquare(p, n);
+        }
+
+        private static double DistanceToSegment(WebDataParser.Point p, WebDataParser.Point ls, WebDataParser.Point le) {
+            return Math.Sqrt(DistanceToSegmentSquared(p, ls, le));
+        }
+
+        public static double DistanceToTargetCell(Attempt attempt) {
+            double scale = attempt.Size == GridSize.Large ? 122.0f : 61.0f;
+            List<Tuple<WebDataParser.Point,WebDataParser.Point>> lineSegments = new List<Tuple<WebDataParser.Point, WebDataParser.Point>>();
+            WebDataParser.Point t = new WebDataParser.Point(attempt.TargetCell.X * scale, attempt.TargetCell.Y * scale);
+            WebDataParser.Point u = new WebDataParser.Point(t.X, t.Y + scale);
+            WebDataParser.Point v = new WebDataParser.Point(t.X + scale, t.Y + scale);
+            WebDataParser.Point w = new WebDataParser.Point(t.X + scale, t.Y);
+            lineSegments.Add(new Tuple<WebDataParser.Point, WebDataParser.Point>(t, u));
+            lineSegments.Add(new Tuple<WebDataParser.Point, WebDataParser.Point>(t, w));
+            lineSegments.Add(new Tuple<WebDataParser.Point, WebDataParser.Point>(u, v));
+            lineSegments.Add(new Tuple<WebDataParser.Point, WebDataParser.Point>(v, w));
+            List<double> distances = new List<double>();
+            foreach(var line in lineSegments) {
+                distances.Add(DistanceToSegment(attempt.Pointer, line.Item1, line.Item2));
+            }
+
+            return distances.Min();
+        }
+
+        public static void GetMissDistances() {
+            var tests = GetTests();
+            using(StreamWriter datawriter = new StreamWriter("missDistances.csv")) {
+                int wrong = 0;
+                datawriter.WriteLine("Technique Distance XDistance YDistance");
+                foreach(var test in tests) {
+                    foreach(var technique in test.Attempts) {
+                        foreach(var attempt in technique.Value) {
+                            if(!attempt.Hit) {
+                                var distances = GetXYDistance(attempt);
+                                if (distances.Item2 == 0 && distances.Item1 == 0) continue;
+                                datawriter.WriteLine($"{GetTechniqueNumber(technique.Key)} {DistanceToTargetCell(attempt)} {distances.Item1} {distances.Item2}");
+                            }
+                            
+                        }
+                    }
+                }
+                Console.WriteLine(wrong);
+            }
+        }
+        public static Tuple<double, double> GetXYDistance(Attempt attempt) {
+            double scale = attempt.Size == GridSize.Large ? 122.0f : 61.0f;
+            double x = attempt.TargetCell.X * scale, y = attempt.TargetCell.Y * scale;
+            double xDistance = 0, yDistance = 0;
+
+            if(attempt.Pointer.X < x) {
+                xDistance = x - attempt.Pointer.X;
+            }
+            else if(attempt.Pointer.X > x + scale) {
+                xDistance = attempt.Pointer.X - (x + scale);
+            }
+
+            if (attempt.Pointer.Y < y) {
+                yDistance = y - attempt.Pointer.Y;
+            } else if (attempt.Pointer.Y > y + scale) {
+                yDistance = attempt.Pointer.Y - (y + scale);
+            }
+
+            return new Tuple<double, double>(xDistance, yDistance);
+        }
     }
 }
