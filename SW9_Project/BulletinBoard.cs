b7935ec61b5e16f29e2e3ef0cc5662e0b4474339
Eric Ruder <ericvruder@gmail.com>
2016-02-15 09:49:14 +0100

    added possibilit for checking for gestures at bullitinboard

--- SW9_Project/BulletinBoard.cs
+++ SW9_Project/BulletinBoard.cs
@@ -8,12 +8,18 @@ using System.Windows.Media;
 namespace SW9_Project {
     class BulletinBoard : CanvasWindow {
 
+        /// <summary>
+        /// I skal hold styr på background, om det skal være et billede eller farve skal ordnes ellers forsvinder griddet ikke
+        /// </summary>
         public BulletinBoard() : base(false) {
-            //I skal hold styr på background, om det skal være et billede eller farve skal ordnes ellers forsvinder griddet ikke
             canvas.Background = Brushes.Tomato;
         }
 
-        public override void ExtendedDraw() {
+        /// <summary>
+        /// This gets called every draw cycle, IE every time the kinect captures a frame. 
+        /// </summary>
+        /// <param name="gesture">Any gestures that were captured. Can be null</param>
+        public override void ExtendedDraw(KinectGesture gesture) {
 
         }
     }
