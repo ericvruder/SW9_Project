8b07443a44ea7f1ffa77bf18f86c2e6c21f621ab
Eric Ruder <ericvruder@gmail.com>
2016-02-18 23:20:54 +0100

    refactored, moved drawing code away from data generator

--- DataSetGenerator/DataGenerator.cs
+++ DataSetGenerator/DataGenerator.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.IO;
 using WebDataParser;
 using SW9_Project;
-using System.Drawing;
 using Newtonsoft.Json;
 
 using Spss;
@@ -13,11 +12,11 @@ namespace DataSetGenerator {
 
     public static class DataGenerator {
 
-        private static List<GestureType> AllTechniques = new List<GestureType> { GestureType.Pinch, GestureType.Swipe, GestureType.Throw, GestureType.Tilt };
-        private static List<GestureDirection> AllDirections = new List<GestureDirection> { GestureDirection.Push, GestureDirection.Pull };
+        public static List<GestureType> AllTechniques = new List<GestureType> { GestureType.Pinch, GestureType.Swipe, GestureType.Throw, GestureType.Tilt };
+        public static List<GestureDirection> AllDirections = new List<GestureDirection> { GestureDirection.Push, GestureDirection.Pull };
 
-        static string TestFileDirectory { get { return ".\\..\\..\\..\\Testlog/"; } }
-        static string DataDirectory
+        public static string TestFileDirectory { get { return ".\\..\\..\\..\\Testlog/"; } }
+        public static string DataDirectory
         {
             get
             {
@@ -230,117 +229,7 @@ namespace DataSetGenerator {
             }
         }
 
-        private static void DrawHitBox(List<Attempt> attempts, string fileName) {
-
-            //61 pixel sized squares, makes it better to look at
-            int cellSize = 61;
-            int bmsize = cellSize * 3;
-
-            Bitmap hitbox = new Bitmap(bmsize, bmsize);
-            Graphics hBGraphic = Graphics.FromImage(hitbox);
-            hBGraphic.FillRectangle(Brushes.White, 0, 0, bmsize, bmsize);
-            hBGraphic.DrawRectangle(new Pen(Brushes.Black, 1.0f), cellSize, cellSize, cellSize, cellSize);
-
-            foreach (var attempt in attempts) {
-                Brush brush = Brushes.Red;
-                float scale = attempt.Size == GridSize.Large ? 122.0f : 61.0f;
-                WebDataParser.Point p = new WebDataParser.Point(attempt.TargetCell.X, attempt.TargetCell.Y);
-                p.X = p.X * scale; p.Y = p.Y * scale;
-                p.X = attempt.Pointer.X - p.X;
-                p.Y = attempt.Pointer.Y - p.Y;
-                if (attempt.Size == GridSize.Large) {
-                    p.X /= 2;
-                    p.Y /= 2;
-                }
-
-                p.X += cellSize;
-                p.Y += cellSize;
-
-                if((p.X > cellSize && p.X < cellSize*2) && (p.Y > cellSize && p.Y < cellSize * 2)) {
-                    brush = Brushes.Green;
-                }
-
-                if (!((p.X < 0) && (p.X >= bmsize)) || !((p.Y < 0) && (p.Y >= bmsize))) {
-                    hBGraphic.FillRectangle(brush, (float)p.X, (float)p.Y, 2, 2);
-                }
-            }
-
-            hBGraphic.Save();
-
-            hitbox.Save(DataDirectory + fileName);
-
-            hBGraphic.Dispose();
-            hitbox.Dispose();
-
-        }
-
-        public static void CreateHitboxes() {
-            var tests = GetTests();
-            Dictionary<GestureType, List<Attempt>> techAttempts = new Dictionary<GestureType, List<Attempt>>();
-            Dictionary<GridSize, List<Attempt>> sizeAttempts = new Dictionary<GridSize, List<Attempt>>();
-            sizeAttempts.Add(GridSize.Large, new List<Attempt>());
-            sizeAttempts.Add(GridSize.Small, new List<Attempt>());
-            foreach (var test in tests) {
-                foreach(var gesture in AllTechniques) {
-                    if (!techAttempts.ContainsKey(gesture)) {
-                        techAttempts.Add(gesture, new List<Attempt>());
-                    }
-                    techAttempts[gesture].AddRange(test.Attempts[gesture]);
-                    sizeAttempts[GridSize.Small].AddRange(from attempt in test.Attempts[gesture]
-                                where attempt.Size == GridSize.Small
-                                select attempt);
-                    sizeAttempts[GridSize.Large].AddRange(from attempt in test.Attempts[gesture]
-                                where attempt.Size == GridSize.Large
-                                select attempt);
-                }
-            }
-
-            DrawHitBox(techAttempts[GestureType.Pinch], "pinch.png");
-            DrawHitBox(techAttempts[GestureType.Swipe], "swipe.png");
-            DrawHitBox(techAttempts[GestureType.Throw], "throw.png");
-            DrawHitBox(techAttempts[GestureType.Tilt], "tilt.png");
-
-            DrawHitBox(sizeAttempts[GridSize.Large], "large.png");
-            DrawHitBox(sizeAttempts[GridSize.Small], "small.png");
-
-            var lp = from attempt in techAttempts[GestureType.Pinch]
-                     where attempt.Size == GridSize.Large
-                     select attempt;
-            DrawHitBox(lp.ToList(), "pinchlarge.png");
-            var sp = from attempt in techAttempts[GestureType.Pinch]
-                     where attempt.Size == GridSize.Small
-                     select attempt;
-            DrawHitBox(sp.ToList(), "pinchsmall.png");
-
-            var ls = from attempt in techAttempts[GestureType.Swipe]
-                     where attempt.Size == GridSize.Large
-                     select attempt;
-            DrawHitBox(ls.ToList(), "swipelarge.png");
-            var ss = from attempt in techAttempts[GestureType.Swipe]
-                     where attempt.Size == GridSize.Small
-                     select attempt;
-            DrawHitBox(ss.ToList(), "swipesmall.png");
-
-            var lti = from attempt in techAttempts[GestureType.Tilt]
-                     where attempt.Size == GridSize.Large
-                     select attempt;
-            DrawHitBox(lti.ToList(), "tiltlarge.png");
-            var sti = from attempt in techAttempts[GestureType.Tilt]
-                     where attempt.Size == GridSize.Small
-                     select attempt;
-            DrawHitBox(sti.ToList(), "tiltsmall.png");
-
-            var lth = from attempt in techAttempts[GestureType.Throw]
-                      where attempt.Size == GridSize.Large
-                      select attempt;
-            DrawHitBox(lth.ToList(), "throwlarge.png");
-            var sth = from attempt in techAttempts[GestureType.Throw]
-                      where attempt.Size == GridSize.Small
-                      select attempt;
-            DrawHitBox(sth.ToList(), "throwsmall.png");
-
-        }
-
+        
         private static double DistanceSquare(WebDataParser.Point v, WebDataParser.Point w) {
             return Math.Pow(v.X - w.X, 2) + Math.Pow(v.Y - w.Y,2);
         }
--- DataSetGenerator/Program.cs
+++ DataSetGenerator/Program.cs
@@ -12,7 +12,7 @@ namespace DataSetGenerator {
             DataGenerator.GenerateJSONDocument();
             DataGenerator.CreateSPSSDocument();
             DataGenerator.CreateCSVDocument();
-            DataGenerator.CreateHitboxes();
+            HitboxDrawer.CreateHitboxes();
         }
     }
 }
