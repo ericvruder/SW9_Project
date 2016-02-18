7bfe259cdb85d78fd95825753d997a919065e894
Eric Ruder <ericvruder@gmail.com>
2016-02-18 13:08:48 +0100

    json doc stuff

--- WebDataParser/Point.cs
+++ WebDataParser/Point.cs
@@ -3,6 +3,10 @@
         public double X { get; set; }
         public double Y { get; set; }
 
+        public override string ToString() {
+            return $"({X},{Y})";
+        }
+
         public Point(double x, double y) {
             X = x;
             Y = y;
