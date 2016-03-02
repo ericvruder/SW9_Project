15ee79cbee62ab3322263128d700c38993832f1a
Elias Ringhauge <Elias Ringhauge>
2016-03-02 22:27:42 +0100

    Added buleting board activity

---
+++ Android/app/src/main/java/com/nui/android/activities/Bboard.java
@@ -0,0 +1,16 @@
+package com.nui.android.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nui.android.R;
+
+public class Bboard extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bboard);
+        setContentView(R.layout.activity_base);
+    }
+}
