ed7c04ad0a7248a63e55755cb4b7aafb69b84c4b
bmla <detailx@gmail.com>
2016-04-04 14:54:38 +0200

    Changed to attemptRepo

--- DataSetGenerator/Program.cs
+++ DataSetGenerator/Program.cs
@@ -8,11 +8,9 @@ namespace DataSetGenerator {
     class Program {
         static void Main(string[] args) {
 
-
-            /*AttemptRepository.RemoveTests(DataSource.Target);
             var oldTest = DataGenerator.GetTests(DataSource.Old);
             var targetTest = DataGenerator.GetTests(DataSource.Target);
-            var fieldTest = DataGenerator.GetTests(DataSource.Field);*/
+            var fieldTest = DataGenerator.GetTests(DataSource.Field);
             DataGenerator.CreateSPSSDocument();
 
             Console.Read();
