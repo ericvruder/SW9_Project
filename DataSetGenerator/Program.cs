904f39a7fd304394b18665c8cdc12f76e3ca12cd
bmla <detailx@gmail.com>
2016-04-18 17:03:46 +0200

    Comment and validity

--- DataSetGenerator/Program.cs
+++ DataSetGenerator/Program.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataSetGenerator {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
 
-                DataGenerator.GenerateCSVDocument(DataSource.Old);
+            //List<Validity> validities = new List<Validity>();
+
+            //string[] files = System.IO.Directory.GetFiles(DataGenerator.DataDirectory, "*.invalid");
+            //foreach (var file in files)
+            //{
+            //    var filename = Path.GetFileNameWithoutExtension(file);
+            //    StreamReader sr = new StreamReader(file);
+            //    using (sr)
+            //    {
+            //        string line = "";
+            //        while ((line = sr.ReadLine()) != null)
+            //        {
+            //            Validity invalid = new Validity(filename, line);
+            //            validities.Add(invalid);
+            //        }
+            //    }
+            //}
+
             //    var attempts = AttemptRepository.GetAttempts(DataSource.Target);
             //    var lAttempts = attempts.Where(x => x.Size == GridSize.Small);
             //    DataVisualizer.DrawHeatMap(lAttempts.ToList(), GridSize.Small, "heatmap.png");
---
+++ DataSetGenerator/Validity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataSetGenerator
+{
+    public class Validity
+    {
+        private int _id;
+        private GestureDirection _direction;
+        private GestureType _type;
+        private int _invalidAttempts;
+
+        public void ValidateAttempts(Test t)
+        {
+            var id = t.ID;
+            foreach (var tech in DataGenerator.AllTechniques)
+            {
+                //Get all miss attempts
+                //get number of invalid attempts from validity file
+                //Apply false to valid property in attempt class on database for for furthest attempts 
+            }
+        }
+
+        public Validity(string id, string line)
+        {
+            string[] info = line.Split(':');
+
+            // push,pinch:3
+            _id = Int32.Parse(id);
+            switch (info[0].Split(',')[0])
+            {
+                case "push":
+                    _direction = GestureDirection.Push;
+                    break;
+                case "pull":
+                    _direction = GestureDirection.Pull;
+                    break;
+                default:
+                    throw new Exception("Direction must be either: push, pull");
+            }
+            switch (info[0].Split(',')[1])
+            {
+                case "pinch":
+                    _type = GestureType.Pinch;
+                    break;
+                case "grab":
+                    _type = GestureType.Pinch;
+                    break;
+                case "swipe":
+                    _type = GestureType.Swipe;
+                    break;
+                case "throw":
+                    _type = GestureType.Throw;
+                    break;
+                case "tilt":
+                    _type = GestureType.Tilt;
+                    break;
+                default:
+                    throw new Exception("Technique must be either: pinch (or grab), swipe, throw, tilt");
+            }
+            _invalidAttempts = Int32.Parse(info[1]);
+
+        }
+    }
+}
